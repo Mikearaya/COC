e810a4f8c13c9a1798c4473f81aa06fa68078c1a
Elsa Teklu <elizate56@gmail.com>
2018-06-16 13:17:57 +0300

    login

--- backend/application/models/Focal_model.php
+++ backend/application/models/Focal_model.php
@@ -3,26 +3,50 @@ class Focal_model extends MY_Model {
     public function __construct() {
         parent::__construct();
         $this->load->database();
+    }/*
+    
+            */
+     public function select_center($contact_person, $password){
+
+        $this->db->where('contact_person',$contact_person);
+        $this->db->where('password',$password);
+        $query = $this->db->get('center');
+
+        if($query->num_rows() > 0){
+            return $query->row_array();
+        }
+        else{
+            return false;
+        }
+     
     }
     
-
-
-        public function save_center($center) {
-            if(!is_null($center['id'])) {
-                return $this->update_center($center);
-            } else {
-                $this->db->insert('center' , $center);
-            }
-            return ($this->db->affected_rows()) ? true : false; 
-        }
-        
-        public function update_center($center) {
-            
-              $this->db->where('id', $center['id']);
-            return $this->db->update('center' , $center);
-        }
-    
+    public function checkOldPass_post($old_password)
+    {
+        $id = $this->input->post('id');
+                $this->db->where('contact_person', $this->session->userdata('contact_person'));
+                $this->db->where('id', $id);
+        $this->db->where('password', $old_password);
+        $query = $this->db->get('center');
+        if($query->num_rows() > 0)
+            return 1;
+        else
+            return 0;
     }
+
+    public function saveNewPass_post($new_pass)
+    {
+        $data = array(
+               'password' => $new_pass
+            );
+        $this->db->where('id', $this->input->post('id'));
+        $this->db->update('center', $data);
+        return true;
+    }
+    
+    
+
+}
 ?>
         
       
