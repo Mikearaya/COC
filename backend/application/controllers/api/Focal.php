3ccaa08b47b23f28db09d6f5ea5b3c14b046a89f
Elsa Teklu <elizate56@gmail.com>
2018-06-20 09:22:56 +0300

    login

--- backend/application/controllers/api/Focal.php
+++ backend/application/controllers/api/Focal.php
@@ -8,67 +8,27 @@ class Focal extends API {
       $this->load->model('focal_model');
 
 }  
-
-//focal person login
-function index_post() {
-
-    $this->load->library('form_validation');
-    $result['success'] = false;
-
-    $this->form_validation->set_rules("contact_person", "Contact Person", "required");
-    $this->form_validation->set_rules("password", "Password", "required");
-    if($this->form_validation->run() === FALSE ) {
-        $this->response($this->validation_errors(), API::HTTP_OK);
-        if($this->focal_model->select_center($contact_person,$password)){
-
-            $session_data = array(
-                
-                'contact_person' => $this->input->post('contact_person'),
-                'password' => $this->input->post('password'), 
-            );
-            $this->session->set_userdata($session_data);
-            return true;
+function index_post(){
+    $data['error']=0;
+    if($_POST){
+        $contact_person=$this->input->post('contact_person',true);
+        $password=$this->input->post('password',true);
+        $focal=$this->focal_model->select_focal($contact_person,$password);
+        if(!$focal){
+            $data['error']=1;
         }
-        else {
-            $this->session->set_flashdata('error','Invalid login. User not found');
-        }
-    } else {
-
-        $this->post();
-       
-        
-      
-}
-   $result['success'] = ($this->focal_model->select_center($contact_person,$password)) ? true : false;
-   $this->response($result, API::HTTP_OK);
-}
-// change password
-public function changePassword()
-{
-    $pass=$this->input->post('old_password');
-    $npass=$this->input->post('newpassword');
-    $rpass=$this->input->post('re_password');
-    if($npass!=$rpass){
-        return "false";
-    }else{
-        $this->db->select('*');
-        $this->db->from('center');
-        $this->db->where('contact_person',$this->session->userdata('contact_person'));
-        $this->db->where('password',md5($pass));
-        $query = $this->db->get();
-        if($query->num_rows()==1){
-            $data = array(
-                           'password' => md5($npass)
-                        );
-            $this->db->where('contact_person', $this->session->userdata('contact_person'));
-            $this->db->update('center', $data); 
-            return "true";
-        }else{
-            return "false";
+        else{
+            $this->session->set_userdata('center_id',$focal['center_id']);
+            $this->session->set_userdata('center_name',$focal['center_name']);
+            $this->response('sucssus');
         }
     }
+
+    echo 'not found';
 }
 
 }
+
+
 
 ?>
--- backend/application/models/Focal_model.php
+++ backend/application/models/Focal_model.php
@@ -3,47 +3,18 @@ class Focal_model extends MY_Model {
     public function __construct() {
         parent::__construct();
         $this->load->database();
-    }/*
-    
-            */
-     public function select_center($contact_person, $password){
-
-        $this->db->where('contact_person',$contact_person);
-        $this->db->where('password',$password);
-        $query = $this->db->get('center');
-
-        if($query->num_rows() > 0){
-            return $query->row_array();
-        }
-        else{
-            return false;
-        }
-     
     }
-    
-    public function checkOldPass_post($old_password)
-    {
-        $id = $this->input->post('id');
-                $this->db->where('contact_person', $this->session->userdata('contact_person'));
-                $this->db->where('id', $id);
-        $this->db->where('password', $old_password);
-        $query = $this->db->get('center');
-        if($query->num_rows() > 0)
-            return 1;
-        else
-            return 0;
+    function select_focal($contact_person,$password){
+        $where=array(
+            'contact_person'=>$contact_person,
+            'password'=>$password
+        );
+        $this->db->select('*')->from('center')->where($where);
+        $query=$this->db->get();
+        return $query->first_row('array');
     }
-
-    public function saveNewPass_post($new_pass)
-    {
-        $data = array(
-               'password' => $new_pass
-            );
-        $this->db->where('id', $this->input->post('id'));
-        $this->db->update('center', $data);
-        return true;
-    }
-    
+ 
+   
     
 
 }
