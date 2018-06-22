a65b0aa51663ed55e21fef9ea9d17cebafa2bb67
Mikael Araya <Mikaelaraya12@gmail.com>
2018-06-22 10:54:31 +0300

    completed dashboard

--- backend/application/models/Dash_model.php
+++ backend/application/models/Dash_model.php
@@ -5,7 +5,7 @@ class Dash_model extends MY_Model {
         $this->load->database();
     }
 
-    public function get_payment(){
+    public function get_payment($center_id){
 
         //$user = $this->session->get_userdata();
         $this->db->select('COUNT(assessment.can_regno) as total');
@@ -13,65 +13,46 @@ class Dash_model extends MY_Model {
         $this->db->where('assessment.paid',1);
        // $this->db->where('assessment.center_code',00);
         $this->db->group_by('assessment.center_code');
-        // $this->db->where('assessment.center_code',  $user['center_code']);
+        $this->db->where('assessment.center_code',  $center_id);
         $query = $this->db->get(); 
-        if($query->num_rows() != 0){
-            return $query->result_array();
-        } else {    
-            return false;
-        }
+        return $query->num_rows();
     }
-    public function get_admission(){
+    public function get_admission($center_id){
 
-      //  $user = $this->session->get_userdata();
         $this->db->select('count(candidate.reg_no) as total');
         $this->db->from('candidate');
         $this->db->where('candidate.print_admission', 'No' );
-       // $this->db->where('assessment.center_code',  $user['center_code']);
         $this->db->join('assessment','candidate.reg_no = assessment.can_regno');
-       // $this->db->where('assessment.center_code',00);
+        $this->db->where('assessment.center_code',$center_id);
         $this->db->group_by('assessment.center_code');
         $query = $this->db->get(); 
-        if($query->num_rows() != 0){
-            return $query->result_array();
-        } else {    
-            return false;
-        }
+        return $query->num_rows();
     }
 
-    public function get_schedule(){
+    public function get_schedule($center_id){
 
         $user = $this->session->get_userdata();
-        $this->db->select('count(schedule.sch_id) as total');
+        $this->db->select('count(candidate_group.gr_id) as total');
         $this->db->from('schedule');
         $this->db->join('candidate_group','schedule.group_no = candidate_group.gr_id','left');
-        //$this->db->where('assessment.center_code',  $user['center_code']);
-       
+        $this->db->join('assessment','assessment.exam_id = candidate_group.exam_id','left');
+        $this->db->where('schedule.center_code', $center_id);
+        $this->db->where('schedule.scheduled_date <=', date('YYYY-mm-dd') );
         $this->db->group_by('candidate_group.gr_id');
         $query = $this->db->get(); 
-        if($query->num_rows() != 0){
-            return $query->result_array();
-        } else {    
-            return false;
-        }}
-        public function get_result(){
 
-            //$user = $this->session->get_userdata();
+    return $query->num_rows();
+    }
+        public function get_result($center_id) {
             $this->db->select('count(assessment.can_regno) as total');
             $this->db->from('assessment');
             $this->db->join('candidate_group','candidate_group.exam_id = assessment.exam_id','left');
             $this->db->join('schedule','schedule.group_no = candidate_group.gr_id','left');
-           //$this->db->join('assessment','candidate.reg_no = assessment.can_regno');
             $this->db->join('center','assessment.center_code = center.center_code','left');
-            //$this->db->where('assessment.center_code',00);
+            $this->db->where('assessment.center_code',  $center_id);
             $this->db->group_by('assessment.center_code');        
-           // $this->db->where('assessment.center_code',  $user['center_code']);
-            $query = $this->db->get(); 
-            if($query->num_rows() != 0){
-                return count($query->result_array());
-            } else {    
-                return false;
-            }
+           
+            return $query->num_rows();
         }
 }
 ?>
