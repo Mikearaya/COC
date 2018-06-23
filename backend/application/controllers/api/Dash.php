6ea2c7b48f0be572c25cc36158bd78c6a2ff8cca
Elsa Teklu <elizate56@gmail.com>
2018-06-23 11:05:22 +0300

    COMMENT

--- backend/application/controllers/api/Dash.php
+++ backend/application/controllers/api/Dash.php
@@ -6,26 +6,26 @@
         parent::__construct($config);
         $this->load->model('dash_model');
     }
-   //get candidate how are not paid
+   //get candidate how are not paid for assessment 
    function payment_get($id = NULL) {
 
     $result = $this->dash_model->get_payment($id);
     $this->response($result,API::HTTP_OK);
 }
 
-//get candidate how are not paid
+//get candidate how don't have admission card
 function admission_get($id = NULL) {
 
     $result = $this->dash_model->get_admission($id);
     $this->response($result,API::HTTP_OK);
 }
-//get candidate how are not paid
+//get new sechedule for assessment 
 function schedule_get($id = NULL) {
 
     $result = $this->dash_model->get_schedule($id);
     $this->response($result,API::HTTP_OK);
 }
-//get candidate how are not paid
+//get candidate new result
 function result_get($id = NULL) {
 
     $result = $this->dash_model->get_result($id);
--- backend/application/models/Dash_model.php
+++ backend/application/models/Dash_model.php
@@ -4,19 +4,19 @@ class Dash_model extends MY_Model {
         parent::__construct();
         $this->load->database();
     }
-
+    //get candidate how are not paid for assessment
     public function get_payment($center_id){
 
-        //$user = $this->session->get_userdata();
+        $user = $this->session->get_userdata();
         $this->db->select('COUNT(assessment.can_regno) as total');
         $this->db->from('assessment');
         $this->db->where('assessment.paid',1);
-       // $this->db->where('assessment.center_code',00);
         $this->db->group_by('assessment.center_code');
         $this->db->where('assessment.center_code',  $center_id);
         $query = $this->db->get(); 
         return $query->num_rows();
     }
+    //get candidate how don't have admission card
     public function get_admission($center_id){
 
         $this->db->select('count(candidate.reg_no) as total');
@@ -28,7 +28,7 @@ class Dash_model extends MY_Model {
         $query = $this->db->get(); 
         return $query->num_rows();
     }
-
+    //get new sechedule for assessment 
     public function get_schedule($center_id){
 
         $user = $this->session->get_userdata();
@@ -43,16 +43,17 @@ class Dash_model extends MY_Model {
         $this->db->group_by('candidate_group.gr_id');
         $query = $this->db->get(); 
 
-    return $query->num_rows();
+        return $query->num_rows();
     }
-        public function get_result($center_id) {
-            $this->db->select('count(assessment.can_regno) as total');
-            $this->db->from('assessment');
-            $this->db->join('candidate_group','candidate_group.exam_id = assessment.exam_id','left');
-            $this->db->join('schedule','schedule.group_no = candidate_group.gr_id','left');
-            $this->db->join('center','assessment.center_code = center.center_code','left');
-            $this->db->where('assessment.center_code',  $center_id);
-            $this->db->group_by('assessment.center_code');        
+    //get candidate new result
+    public function get_result($center_id) {
+        $this->db->select('count(assessment.can_regno) as total');
+        $this->db->from('assessment');
+        $this->db->join('candidate_group','candidate_group.exam_id = assessment.exam_id','left');
+        $this->db->join('schedule','schedule.group_no = candidate_group.gr_id','left');
+        $this->db->join('center','assessment.center_code = center.center_code','left');
+        $this->db->where('assessment.center_code',  $center_id);
+        $this->db->group_by('assessment.center_code');        
            
             return $query->num_rows();
         }
