2b7e6532f2ae28538f427078a15a4ca8f840ceaa
Mikael Araya <Mikaelaraya12@gmail.com>
2018-06-25 15:58:55 +0300

    password change completed

--- backend/application/controllers/api/Password.php
+++ backend/application/controllers/api/Password.php
@@ -9,42 +9,47 @@ class Password extends API {
 
 }  
 // password change function
-function change_post($centerId = NULL){
+function change_post(){
     $this->load->library('form_validation');
    
 
     $this->form_validation->set_rules('password','password','required');
-    $this->form_validation->set_rules('newpassword','newpassword','required');
-    $this->form_validation->set_rules('repassword','repassword','required');
+    $this->form_validation->set_rules('newpassword','new password','required');
+    $this->form_validation->set_rules('repassword','New Password Doesnt Match','required');
     if($this->form_validation->run()) {
-    
+  
      $cur_password= $this->input->post('password');
      $new_password= $this->input->post('newpassword');
      $re_password= $this->input->post('repassword');
      
     
-     $password=$this->password_model->get_password($centerId);
-     
+     $password= $this->password_model->get_password($this->session->userdata('center_id'));
      if($password->password == $cur_password){
         if($new_password == $re_password){
-            if($this->password_model->update_password($new_password,$centerId)){
-                echo 'password updated';
+            if($this->password_model->update_password($new_password, $this->session->userdata('center_id'))){
+                $result['success'] = true;
             }
             else{
-                echo 'failed to update password';
+                $result['success'] = false;
+                $result['message'] = 'failed to update password';
             }
         }
         else{
-            echo 'sorry new password & repassword is not matching';
+            $result['success'] = false;
+           $result['message'] = 'sorry new password & repassword is not matching';
         }
      }else{
-         echo 'sorry current password is not matching';
+        $result['success'] = false;
+        $result['message'] = 'sorry current password is not matching';
      }
    
     }
     else{
-        echo 'error';
+        $result['message'] = $validation_errors();
+        $result['success'] = false;
     }
+
+    $this->response($result, API::HTTP_OK);
 }
 }
 
