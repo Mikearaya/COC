ef14a9bdbffe5f7f7c13d60ee81f02b04326a7db
Elsa Teklu <elizate56@gmail.com>
2018-06-26 16:55:28 +0300

    peginat

--- backend/application/controllers/api/Result.php
+++ backend/application/controllers/api/Result.php
@@ -7,12 +7,11 @@ class Result extends API {
   public function __construct($config = 'rest') {
     parent::__construct($config);
     $this->load->model('result_model');
-    $this->load->library('pagination');
   }
 //get candidate result
   function index_get($id = NULL) {
-    
-    $result['result'] = $this->result_model->get_result($id );
+
+    $result['result'] = $this->result_model->get_result($id);
     $result['columns']=[];
     
     if(count($result)>0) {
@@ -24,23 +23,8 @@ class Result extends API {
 
 //get candidate result by group
   function group_result_get($id = NULL) {
-    $config = array();
-    $config['base_url'] = 'http://localhost/smart_coc/backend/index.php/api/result';
-    $config['total_rows'] = $this->result_model->get_group_result($id);
-    $config['per_page'] = 10;
-    $config['uri_segment']=3;
-    $choice = $config['total_rows']/$config['per_page'];
-    $config['num_links']= round($choice);
-
-$this->pagination->initialize($config);
-    $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-    
-    $result['result'] = $this->result_model->get_group_result($config['per_page'],$page);
-    $result ['links'] = $this->pagination->create_links ();
-  
+    $result = $this->result_model->get_group_result($id);
     $this->response($result, API::HTTP_OK);
-    
-
   }
 }
 ?>
